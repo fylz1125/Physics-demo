c01861a97a6115d82be598d92090e5deb4276b9b
lerry <fylz1125@163.com>
2019-09-11 23:06:30 +0800

    update

--- assets/Script/physicSetting/PhysicsBound.ts
+++ assets/Script/physicSetting/PhysicsBound.ts
@@ -7,24 +7,23 @@ const {ccclass, property} = cc._decorator;
 @ccclass
 export default class PhysicsBound extends cc.Component {
 
+    @property
+    mouseJoint = true;
+
 
     onLoad() {
-        // let physicsManager = cc.director.getPhysicsManager();
-        // physicsManager.enabled = true;
-        // physicsManager.debugDrawFlags =
-            // @ts-ignore
-            // cc.PhysicsManager.DrawBits.e_jointBit |
-            // @ts-ignore
-            // cc.PhysicsManager.DrawBits.e_shapeBit |
-            // @ts-ignore
-            // cc.PhysicsManager.DrawBits.e_aabbBit;
-
         let width = this.node.width;
         let height = this.node.height;
 
         let node = new cc.Node();
         let body = node.addComponent(cc.RigidBody);
         body.type = cc.RigidBodyType.Static;
+
+        if (this.mouseJoint) {
+            // add mouse joint
+            let joint = node.addComponent(cc.MouseJoint);
+            joint.mouseRegion = this.node;    
+        }
 
         this.addBound(node, 0, height / 2, width, 10);
         this.addBound(node, 0, -height / 2, width, 10);
