5ba8c2fcfbdbabfa0beb7328b31693fb44e06d0b
lerry <fylz1125@163.com>
2019-09-12 21:24:22 +0800

    update

--- assets/Script/conveyorBelt/Conveyor.ts
+++ assets/Script/conveyorBelt/Conveyor.ts
@@ -5,7 +5,7 @@
 const {ccclass, property} = cc._decorator;
 
 @ccclass
-export default class ConveyorBelt extends cc.Component {
+export default class Conveyor extends cc.Component {
 
     @property
     tangentSpeed = 5;
